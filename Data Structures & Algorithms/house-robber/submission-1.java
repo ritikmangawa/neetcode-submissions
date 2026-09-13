c14ae6889e6993d8ed5c54e6d852fc390f35b375
RITIK MANGAWA <ritikchoudhary0812@gmail.com>
2026-09-13 22:05:40 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        if(n > 1){
+            dp[1] = Math.max(nums[1], nums[0]);
+        }
+        for(int  i = 2; i < n; i++){
+            dp[i] = Math.max(dp[i-1] , dp[i-2] + nums[i]);
+        }
+        return dp[n - 1];
+        
+    }
+}
